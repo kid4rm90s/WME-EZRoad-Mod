bc3c80566a51e0f57654551f0dcfe62a593b0923
kid4rm90s <165776943+kid4rm90s@users.noreply.github.com>
2026-03-01 18:01:55 +0800

    Update WME EZRoad Mod beta.user.js

--- WME EZRoad Mod beta.user.js
+++ WME EZRoad Mod beta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME EZRoad Mod Beta
 // @namespace    https://greasyfork.org/users/1087400
-// @version      2.6.8.6
+// @version      2.6.8.7
 // @description  Easily update roads
 // @author       https://greasyfork.org/en/users/1087400-kid4rm90s
 // @include 	   /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
@@ -28,8 +28,7 @@
 
 (function main() {
   ('use strict');
-  const updateMessage = `<strong>Version 2.6.8.6 - 2026-02-20:</strong><br>
-    - Restored paved or unpaved function to DOM since SDK methods do not provide immediate feedback.<br>
+  const updateMessage = `<strong>Version 2.6.8.7 - 2026-03-01:</strong><br>
     - Fixed found bug fixes.<br>
 <br>`;
   const scriptName = GM_info.script.name;
@@ -3714,7 +3713,8 @@
                 margin-right: 5px;
             }
             .ezroadsmod-radio-container input.road-speed {
-                width: 60px;
+                width: 50px;
+                margin-right: 5px;
             }
             .ezroadsmod-reset-button {
                 margin-top: 20px;
